80ea706cd5399ef5f2af1eb0e19384e033b426a2
joaoasilva <joaoasilva@gmail.com>
2022-05-14 00:16:23 +0100

    Adding detekt to analyse code smells

--- app/src/main/java/com/photobox/joaocv/ui/theme/Color.kt
+++ app/src/main/java/com/photobox/joaocv/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.photobox.joaocv.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+const val PURPLE200_HEX = 0xFFBB86FC
+const val PURPLE500_HEX = 0xFF6200EE
+const val PURPLE700_HEX = 0xFF3700B3
+const val TEAL200_HEX = 0xFF03DAC5
+
+val Purple200 = Color(PURPLE200_HEX)
+val Purple500 = Color(PURPLE500_HEX)
+val Purple700 = Color(PURPLE700_HEX)
+val Teal200 = Color(TEAL200_HEX)
